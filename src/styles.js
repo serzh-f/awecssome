2954239ae968536abcdbbd8b0f7ea38fa63cadc2
Pavel Lautsevich <zigzag.mcquack@gmail.com>
2016-12-22 18:31:40 +0300

    Refactored breadcrumbs, changed syntax to mixin

--- src/styles.js
+++ src/styles.js
@@ -8,3 +8,4 @@ import './modules/library/common/button/_x-small/_x-small.css';
 import './modules/library/common/button/_ghost/_ghost.css';
 
 import './modules/library/common/breadcrumbs/breadcrumbs.css';
+import './modules/library/common/breadcrumbs/_incomplete/_incomplete.css';
