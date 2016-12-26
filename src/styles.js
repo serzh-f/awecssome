3599075e2ef085efef23a5acd21bfd13d1257617
Pavel Lautsevich <zigzag.mcquack@gmail.com>
2016-12-26 16:55:08 +0300

    Added produc cart with modifiers.

--- src/styles.js
+++ src/styles.js
@@ -6,3 +6,5 @@ import './modules/library/common/button/_large/_large.css';
 import './modules/library/common/button/_small/_small.css';
 import './modules/library/common/button/_x-small/_x-small.css';
 import './modules/library/common/button/_ghost/_ghost.css';
+
+import './modules/library/common/product-card/product-card.css';
